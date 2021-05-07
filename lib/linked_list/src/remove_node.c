26a77452a4a7f5eff299c222a463490fb78a1f2c
pierre1754 <pierre1754@outlook.com>
2021-05-07 21:59:19 +0200

    ADDED: remake remove node

--- lib/linked_list/src/remove_node.c
+++ lib/linked_list/src/remove_node.c
@@ -7,18 +7,14 @@
 
 #include "linked_list.h"
 
-list_node_t *remove_node(list_node_t *node)
+void remove_node(list_node_t *node)
 {
     if (!node)
-        return NULL;
+        return;
     if (node->last)
         node->last->next = node->next;
     if (node->next)
         node->next->last = node->last;
     node->last = NULL;
     node->next = NULL;
-    if (node->last)
-        return node->last;
-    else return node->next;
-    return node;
 }
