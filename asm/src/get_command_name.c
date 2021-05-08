60cafff19e64874ba0826d631bdd9471befbbbec
Pierre <pierre.mauger@epitech.eu>
2021-05-08 03:31:36 +0200

    FEAT: add check params size

--- asm/src/get_command_name.c
+++ asm/src/get_command_name.c
@@ -11,7 +11,8 @@ char *get_command_name(char *buffer, size_t adv)
 {
     size_t len = 0;
 
-    for (; buffer[adv + len] && buffer[adv + len] !=  ' ' && buffer[adv + len] !=  ':'; len++);
+    for (; buffer[adv + len] && buffer[adv + len] !=  ' ' &&
+    buffer[adv + len] != ':'; len++);
     for (int i = 0; i < 16; i++)
         if (!bstrncmp(buffer + adv, op_tab[i].mnemonique, len))
             return bstrdup(op_tab[i].mnemonique);
