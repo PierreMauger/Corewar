86e5de1be42ebea5745e032fa2371dfd127d42c9
xavier <xavier.tonnellier@epitech.eu>
2021-05-11 16:18:39 +0200

    FIXED: skip commas and spaces

--- asm/src/get_command_params.c
+++ asm/src/get_command_params.c
@@ -11,17 +11,11 @@ size_t get_param_len(char *buffer, size_t *adv)
 {
     size_t compt = 0;
     size_t len = 0;
-    size_t count_vir = 0;
 
-    if (buffer[*adv] == ',') {
-        (*adv)++;
-        count_vir++;
-    }
-    for (; buffer[*adv] == ' '; (*adv)++);
-    if (buffer[*adv] == ',' && count_vir >= 1)
-        return (0);
+    for (; buffer[*adv] == ' ' || buffer[*adv] == ','; (*adv)++);
     compt = *adv;
-    while (buffer[compt] && buffer[compt] != ' ' && buffer[compt] != '\n') {
+    while (buffer[compt] && buffer[compt] != ' '
+        && buffer[compt] != ',' && buffer[compt] != '\n') {
         compt++;
         len++;
     }
@@ -32,20 +26,15 @@ char *get_one_param(char *buffer, size_t *adv)
 {
     size_t len = get_param_len(buffer, adv);
     size_t fill_tab = 0;
-    char *param = NULL;
+    char *param = malloc(sizeof(char) * (len + 1));
 
-    if (len == 0)
-        return (NULL);
-    param = malloc(sizeof(char) * (len + 1));
     if (param == NULL)
         return (NULL);
-    for (; buffer[*adv] && buffer[*adv] != ' ' && buffer[*adv] != '\n';
-    (*adv)++, fill_tab++) {
-        if (buffer[*adv] == ',')
-            break;
+    for (; buffer[*adv] && buffer[*adv] != ' ' && buffer[*adv] != ','
+        && buffer[*adv] != '\n'; (*adv)++, fill_tab++) {
         param[fill_tab] = buffer[*adv];
     }
-    for (; buffer[*adv] == ' '; (*adv)++);
+    for (; buffer[*adv] == ' ' && buffer[*adv] == ','; (*adv)++);
     param[fill_tab] = '\0';
     return param;
 }
@@ -55,7 +44,9 @@ char **get_command_params(char *buffer, size_t adv)
     char **params_tab = malloc(sizeof(char *) * MAX_ARGS_NUMBER);
     int i = 0;
 
-    for (; buffer[adv] && buffer[adv] != '\n'; i++) {
+    if (!params_tab)
+        return (NULL);
+    for (; buffer[adv] && buffer[adv] != '\n'; i++, adv++) {
         params_tab[i] = get_one_param(buffer, &adv);
         if (params_tab[i] == NULL)
             return (NULL);
