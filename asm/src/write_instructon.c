183701f45a216849433ecb6d94457e527327d2da
Pierre <pierre.mauger@epitech.eu>
2021-05-14 14:24:34 +0200

    FEAT: parameters writing in big endian

--- asm/src/write_instructon.c
+++ asm/src/write_instructon.c
@@ -7,15 +7,53 @@
 
 #include "asm.h"
 
+int swap_endian_4(int val)
+{
+    return (((val) & 0xff000000) >> 24) |
+           (((val) & 0x00ff0000) >>  8) |
+           (((val) & 0x0000ff00) <<  8) |
+           (((val) & 0x000000ff) << 24);
+}
+
+int swap_endian_2(int val)
+{
+    return (((val) & 0xff00) >>  8) |
+           (((val) & 0x00ff) <<  8);
+}
+
+
+void write_dir(int fd, char *param, size_t name_id, list_t *list)
+{
+    int res = batoi(param);
+
+    if (is_num(param)) {
+        if (name_id > 9 && name_id < 13) {
+            res = swap_endian_2(res);
+            write(fd, &res, 2);
+        }
+        else {
+            res = swap_endian_4(res);
+            write(fd, &res, 4);
+        }
+    }
+    if (is_label(param, list))
+        write(fd, "\0\0", 2);
+}
+
 void write_params(int fd, command_t *com, list_t *list)
 {
+    int res = 0;
+
     for (size_t i = 0; i < barray_len(com->params); i++) {
         if (is_reg(com->params[i]))
             bdprintf(fd, "%c", (char)batoi(com->params[i] + 1));
-        if (is_num(com->params[i]))
-            bdprintf(fd, "%d", batoi(com->params[i]));
+        if (is_num(com->params[i])) {
+            res = batoi(com->params[i]);
+            res = swap_endian_2(res);
+            write(fd, &res, 2);
+        }
         if (is_dir(com->params[i], list))
-            /* different dir sizes (with labels) */;
+            write_dir(fd, com->params[i] + 1, get_id(com->name), list);
     }
 }
 
