4d1f941e123865b66f58e88b2e5b68aacf611322
Xavier Tonnellier <xaviertonnellier@outlook.fr>
2021-05-16 14:55:43 +0200

    FIX : multiples spaces or commas after arguments

--- asm/src/get_command_params.c
+++ asm/src/get_command_params.c
@@ -22,12 +22,15 @@ size_t get_param_len(char *buffer, size_t *adv)
     return len;
 }
 
-char *get_one_param(char *buffer, size_t *adv)
+char *get_one_param(char *buffer, size_t *adv, bool *err)
 {
     size_t len = get_param_len(buffer, adv);
     size_t fill_tab = 0;
-    char *param = malloc(sizeof(char) * (len + 1));
+    char *param = NULL;
 
+    if (len == 0)
+        *err = true;
+    param = malloc(sizeof(char) * (len + 1));
     if (!param)
         return (NULL);
     for (; buffer[*adv] && buffer[*adv] != ' ' && buffer[*adv] != ','
@@ -39,17 +42,28 @@ char *get_one_param(char *buffer, size_t *adv)
     return param;
 }
 
+static void check_bool_err(bool *err, char **params_tab, int *i)
+{
+    if (*err == true) {
+        free(params_tab[*i]);
+        (*i)--;
+        *err = false;
+    }
+}
+
 char **get_command_params(char *buffer, size_t adv)
 {
     char **params_tab = malloc(sizeof(char *) * MAX_ARGS_NUMBER);
     int i = 0;
+    bool err = false;
 
     if (!params_tab)
         return (NULL);
     for (; buffer[adv] && buffer[adv] != '\n'; i++, adv++) {
-        params_tab[i] = get_one_param(buffer, &adv);
+        params_tab[i] = get_one_param(buffer, &adv, &err);
         if (params_tab[i] == NULL)
             return (NULL);
+        check_bool_err(&err, params_tab, &i);
         if (buffer[adv] == '\n') {
             i++;
             break;
