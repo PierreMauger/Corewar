578259bde154eb6bbaf7cf6614e77741e4021778
pierre1754 <pierre1754@outlook.com>
2021-05-18 00:42:51 +0200

    FIXED: forbidden func

--- corewar/src/init_struct/set_all.c
+++ corewar/src/init_struct/set_all.c
@@ -29,17 +29,6 @@ bool set_file(parsing_t *pars_temp)
     return 0;
 }
 
-// bool set_file(parsing_t *pars_temp)
-// {
-//     pars_temp->file = bread_file_len(pars_temp->name, READ_SIZE);
-//     if (!pars_temp->file || !pars_temp->file->file || !pars_temp->file->len) {
-//         if (pars_temp->file)
-//             free(pars_temp->file);
-//         return 1;
-//     }
-//     return 0;
-// }
-
 void set_coord_champion(parsing_t *pars_temp, size_t espacement)
 {
     static size_t it = 0;
