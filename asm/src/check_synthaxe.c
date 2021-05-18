3c46c0a6f346f4ad01a8d59fb50e98975694a736
Xavier Tonnellier <xaviertonnellier@outlook.fr>
2021-05-18 04:52:50 +0200

    FIX : replace while/for

--- asm/src/check_synthaxe.c
+++ asm/src/check_synthaxe.c
@@ -9,8 +9,7 @@
 
 void skip_comment(char *buffer, size_t *adv)
 {
-    while (buffer[*adv] && buffer[*adv] != '\n')
-        (*adv)++;
+    for (; buffer[*adv] && buffer[*adv] != '\n'; (*adv)++);
 }
 
 int check_after_name(char *buffer, size_t *adv, char *param_name)
@@ -18,38 +17,35 @@ int check_after_name(char *buffer, size_t *adv, char *param_name)
     *adv += bstrlen(param_name);
     if (buffer[*adv] == '"')
         return (1);
-    while (buffer[*adv] && buffer[*adv] != '"') {
+    for (; buffer[*adv] && buffer[*adv] != '"'; (*adv)++) {
         if (buffer[*adv] != ' ' && buffer[*adv] != '\t') {
             return (1);
         }
-        (*adv)++;
     }
     return (0);
 }
 
 int go_to_name(char *buffer, size_t *adv, char *param_name)
 {
-    while (buffer[*adv]
-        && bstrncmp(buffer + *adv, param_name, bstrlen(param_name)) != 0) {
+    for (; buffer[*adv] && bstrncmp(
+        buffer + *adv, param_name, bstrlen(param_name)) != 0; (*adv)++) {
         if (buffer[*adv] != ' ' && buffer[*adv] != '\t'
             && buffer[*adv] != '\n' && buffer[*adv] != ',') {
             return (1);
         }
-        (*adv)++;
     }
     return (0);
 }
 
 int check_new_line(char *buffer, size_t *adv)
 {
-    while (buffer[*adv]) {
+    for (; buffer[*adv]; (*adv)++) {
         if (buffer[*adv] == '#')
             skip_comment(buffer, adv);
         if (buffer[*adv] != ' ' && buffer[*adv] != ','
             && buffer[*adv] != '\t' && buffer[*adv] != '\n') {
             return (1);
         }
-        (*adv)++;
     }
     return (0);
 }
