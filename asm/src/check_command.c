6d6a696d31d5bdb14b32f47f5a6c17baea826def
Pierre <pierre.mauger@epitech.eu>
2021-05-19 10:31:49 +0200

    FEAT: fix name length

--- asm/src/check_command.c
+++ asm/src/check_command.c
@@ -38,11 +38,9 @@ size_t check_command(list_t *list)
     foreach(list->head, temp) {
         temp_com = (command_t *)temp->data;
         id = get_id(temp_com->name);
-        if ((int)barray_len(temp_com->params) !=
-        op_tab[id].nbr_args)
+        if ((int)barray_len(temp_com->params) != op_tab[id].nbr_args)
             return 0;
-        if (!check_size(temp_com, op_tab[id].type,
-        list))
+        if (!check_size(temp_com, op_tab[id].type, list))
             return 0;
     }
     return 1;
