7ac537ad7b48273e1b4e97db686ea81b22a8c41d
Xavier Tonnellier <xaviertonnellier@outlook.fr>
2021-05-19 21:40:55 +0200

    FIX: norme, more then 20 lines

--- asm/src/get_command_name.c
+++ asm/src/get_command_name.c
@@ -17,8 +17,10 @@ char *get_command_name(char *buffer, size_t *adv)
     buffer[*adv + len] != '\t' && buffer[*adv + len] != ',' &&
     buffer[*adv + len] != ':'; len++);
     for (int i = 0; i < 16; i++) {
-        if (!bstrncmp(buffer + *adv, op_tab[i].mnemonique, len))
+        if (!bstrncmp(buffer + *adv, op_tab[i].mnemonique, len)) {
+            *adv += bstrlen(op_tab[i].mnemonique);
             return bstrdup(op_tab[i].mnemonique);
+        }
     }
     return NULL;
 }
